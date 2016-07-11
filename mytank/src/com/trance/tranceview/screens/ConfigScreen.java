7afc62e088d6d7cb254daa2e5cf8b8e02999e4d6
zhang <zhang@192.168.0.100>
2016-07-11 21:36:18 +0800

    config

---
+++ mytank/src/com/trance/tranceview/screens/ConfigScreen.java
@@ -0,0 +1,12 @@
+package com.trance.tranceview.screens;
+
+import com.badlogic.gdx.ScreenAdapter;
+
+public class ConfigScreen extends ScreenAdapter {
+	
+	/**
+	 * track distance
+	 */
+	public static int trackDistance = 100;
+	
+}
