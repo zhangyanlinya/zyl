ded12768fdc82971890f9bcec59c3cd4a5145288
Administrator <Administrator@PC-20160214RYYC>
2016-10-28 18:12:43 +0800

    pro

--- mytank/src/com/trance/common/socket/constant/CodecConstant.java
+++ mytank/src/com/trance/common/socket/constant/CodecConstant.java
@@ -33,11 +33,11 @@ public interface CodecConstant {
 	/**
 	 * flash策略请求信息
 	 */
-	final byte[] FLASH_POLICY_REQUEST = "<policy-file-request/>".getBytes(CHARSET);
+	//final byte[] FLASH_POLICY_REQUEST = "<policy-file-request/>".getBytes(CHARSET);
 	
 	/**
 	 * flash策略响应信息
 	 */
-	final byte[] FLASH_POLICY_RESPONSE = "<?xml version=\"1.0\"?><cross-domain-policy><site-control permitted-cross-domain-policies=\"all\"/><allow-access-from domain=\"*\" to-ports=\"*\"/></cross-domain-policy>\0".getBytes(CHARSET);
+	//final byte[] FLASH_POLICY_RESPONSE = "<?xml version=\"1.0\"?><cross-domain-policy><site-control permitted-cross-domain-policies=\"all\"/><allow-access-from domain=\"*\" to-ports=\"*\"/></cross-domain-policy>\0".getBytes(CHARSET);
 
 }
